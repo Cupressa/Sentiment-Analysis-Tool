6bf062cc8cf3eb2fd0da305b75dae48d831a0093
Cupressa <jonahcl1583@gmail.com>
2024-09-11 11:49:43 -0500

    Updated sentimentAnalysisTool.py

--- sentimentAnalysisTool.py
+++ sentimentAnalysisTool.py
@@ -1,11 +1,33 @@
 from openai import OpenAI
 from tkinter import filedialog as fd
+import datetime
+import csv
+
+# Jonah Lum
+# 7/5/24
+
+"""
+Changes From V1
+- Generalized format of file input
+  - Can group responses by ip even if input file is not sorted
+  - Utilizes csv library to accommodate for csv file formatting
+    - Note: file should contain the fields in the following order: IP/ID, term, response
+- Generalized name of output file
+- Use of GPT-4o
+
+Future work for V3
+- Confirmation of fields
+- Allow user-input prompts? or selection of prompts for term/term+response analysis
+- Selection of GPT model?
+- Preventing invalid runs from occurring
+"""
+
 client = OpenAI()
 
 def get_sentiment(response):
-    systemPrompt = "Each of the following 5 lines contains a word choice followed by an explanation for the choice. For each line, provide a sentiment analysis score for the word between 0.00-1.00 (to two decimal places), and then an adjusted score for the word based on the explanation."
+    systemPrompt = "Each of the following 5 lines contains a word choice, sometimes followed by an explanation for the choice. For each line, provide a sentiment analysis score for the word between 0.00-1.00 (to two decimal places), and then an adjusted score for the word based on the explanation. Include your confidence in the accuracy of that score (low, medium, high). Additionally, provide a carefully crafted contextual explanation for the sentiment score that is related to the meaning of the text."
     completion = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
+        model = "gpt-4o",
         messages = [
             {
                 "role": "system",
@@ -19,36 +41,33 @@ def get_sentiment(response):
         )
     return completion.choices[0].message.content
 
-def sortedDataReader():
+def read_data():
     filePath = fd.askopenfilename()
-    inFile = open(filePath, 'r')
-    data = inFile.readlines()
-    inFile.close()
-    return data[1:]
+    with open(filePath, 'r', encoding="UTF-8-sig") as file:
+        reader = csv.DictReader(file)
+        responseDict = {}
+        fields = reader.fieldnames
+        for line in reader:
+            if(line[fields[0]] in responseDict.keys()):
+                responseDict[line[fields[0]]] = responseDict[line[fields[0]]] + '"' + line[fields[1]].replace("\n", "") + '","' + line[fields[2]].replace("\n", "") + '"\n'
+            else:
+                responseDict[line[fields[0]]] = '"' + line[fields[1]].replace("\n", "") + '","' + line[fields[2]].replace("\n", "") + '"\n'
+        return responseDict
 
-def analyze_data(data):
-    prevIP = ""
-    responseString = ""
+def analyze_data(responseDict, usingAPI):
     sentiments = []
-    for line in data:
-        line = line.replace(",", "").split("|")
-        IP = line[2]
-        if(prevIP == IP):
-            responseString += '"' + line[0].replace("\n", "") + '","' + line[1].replace("\n", "") + '"\n'
+    for key in responseDict.keys():
+        if(usingAPI):
+            sentiments.append("\n" + key + "\n")
+            sentiments.append(str(get_sentiment(responseDict[key])))
         else:
-            #sentiments.append(responseString)
-            sentiments.append("\n" + IP)
-            sentiments.append(str(get_sentiment(responseString)))
-            prevIP = IP
-            responseString = '"' + line[0].replace("\n", "") + '","' + line[1].replace("\n", "") + '"\n'
-    #sentiments.append(responseString)
-    sentiments.append("\n" + IP)
-    sentiments.append(str(get_sentiment(responseString)))
+            sentiments.append(key + "\n")
+            sentiments.append(responseDict[key])
     return sentiments
-
+        
 def main():
-    outFile = open("CARMA_Analysis.txt", "w")
-    sentiments = analyze_data(sortedDataReader())
+    sentiments = analyze_data(read_data(), True)
+    outFile = open("PDT_Analysis_" + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S").replace(" ", "_").replace("/", "-").replace(":", ";") + ".txt", "w", encoding="UTF-8-sig")
     outFile.writelines(sentiments)
     outFile.close()
 
